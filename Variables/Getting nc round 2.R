17a5767b5f2a0199dacc5cad88dfead2926d39b8
em1105 <em1105@rstudio01n.isad.isadroot.ex.ac.uk>
2026-08-29 13:35:47 +0100

    Initial commit: leopard seal habitat model project

---
+++ Variables/Getting nc round 2.R
@@ -0,0 +1,54 @@
+system("which conda")
+system("which mamba")
+system("conda info --envs")
+system("ls ~")
+system("ls ~/miniforge3")
+system("ls ~/mambaforge")
+system("ls ~/micromamba")
+system("source ~/miniforge3/etc/profile.d/conda.sh && conda --version")
+system("~/miniforge3/bin/conda --version")
+system("~/miniforge3/bin/conda env list")
+system("~/miniforge3/bin/conda run -n copernicus_env python --version")
+system("~/miniforge3/bin/conda env list")
+system("~/miniforge3/bin/conda run -n copernicus python --version")
+system("~/miniforge3/bin/conda list -n copernicus")
+system("~/miniforge3/bin/conda install -n copernicus python=3.10 -c conda-forge -y")
+system("~/miniforge3/bin/conda run -n copernicus python --version")
+system("~/miniforge3/bin/conda install -n copernicus -c conda-forge copernicusmarine -y")
+system("~/miniforge3/bin/conda run -n copernicus copernicusmarine --version")
+
+
+
+
+
+path_copernicusmarine <- "~/miniforge3/bin/conda"
+
+output_directory <- "/home2/ISAD/em1105/2026_Project/Variables"
+dir.create(output_directory, showWarnings = FALSE, recursive = TRUE)
+
+command <- paste(
+  "~/miniforge3/bin/conda run -n copernicus copernicusmarine subset",
+  "--dataset-id cmems_mod_glo_phy_my_0.083deg_P1D-m",
+  "--variable siconc",
+  "--variable sithick",
+  "--variable mlotst",
+  "--variable thetao",
+  "--variable uo",
+  "--variable vo",
+  "--variable zos",
+  "--minimum-longitude -94",
+  "--maximum-longitude -24",
+  "--minimum-latitude -75",
+  "--maximum-latitude -44",
+  "--start-datetime 2018-01-01T00:00:00",
+  "--end-datetime 2019-10-31T00:00:00",
+  "--minimum-depth 0.49402499198913574",
+  "--maximum-depth 0.49402499198913574",
+  "-o", shQuote(output_directory),
+  sep = " "
+)
+
+system(command)
+
+
+
